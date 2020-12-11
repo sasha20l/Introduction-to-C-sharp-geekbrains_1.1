0f80a3b5a69de1960685f2c412b6cdc511384b21
Александр Марфицын <nonesasha225@gmail.com>
2020-12-11 09:59:30 +0500

    Попытка два

---
+++ Hello, World/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello__World
+{
+    class Program
+    {
+        private static string name;
+        static void Main(string[] args)
+        {
+            name = Name();
+            Date();
+            End();
+
+            string Name()
+            {
+                Console.WriteLine("Enter, you name!");
+                return Console.ReadLine();
+
+            }
+
+            void Date()
+            {
+                Console.WriteLine($"Привет, {name}, сегодня [{DateTime.Now}]");
+            }
+
+            void End()
+            {
+                for (int i = 10; i > -1; i--)
+                {
+                    Console.Write($" {i} секунд до закрытия программы");
+                    Console.Write("\r");
+                    Thread.Sleep(1000);
+                }
+
+            }
+
+        }
+        
+    }
+}
